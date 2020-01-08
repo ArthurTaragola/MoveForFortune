f5dcca758a7603ba73d61e1b3dcfa6cf694ce03c
ArthurTaragola <arthur.taragola@student.howest.be>
2020-01-08 11:40:09 +0100

    database online zetten

---
+++ MoveForFortune/OphalenData.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using MoveForFortune.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MoveForFortune
+{
+    public static class OphalenData
+    {
+        [FunctionName("OphalenData")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("ServerConnectionString");
+            List<Leerkracht> LeerkrachtList = new List<Leerkracht>();
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Connection = connection;
+                        sqlCommand.CommandText = "SELECT * FROM Leerkracht"; //SQL command
+                        SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+                        // while not always needed
+                        while (await reader.ReadAsync())
+                        {
+                            LeerkrachtList.Add(new Leerkracht()
+                            {
+                                LeerkrachtId = int.Parse(reader["LeerkrachtId"].ToString()),
+                                Voornaam = reader["Voornaam"].ToString(),
+                                Naam = reader["Naam"].ToString(),
+                                Email = reader["Email"].ToString(),
+                                Wachtwoord = reader["Wachtwoord"].ToString(),
+                                ThemaId = int.Parse(reader["ThemaId"].ToString()),
+                            });
+                        }
+                    }
+                }
+                return new OkObjectResult(LeerkrachtList);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
